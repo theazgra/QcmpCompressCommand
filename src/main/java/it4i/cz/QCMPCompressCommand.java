f289f653d7285ed2ae4ac0b995cd773583c9b998
Vojtech Moravec <vojtech.moravec.st@vsb.cz>
2020-06-29 12:51:57 +0200

    Basic options for command.

--- src/main/java/it4i/cz/QCMPCompressCommand.java
+++ src/main/java/it4i/cz/QCMPCompressCommand.java
@@ -8,18 +8,23 @@
 
 package it4i.cz;
 
+import azgracompress.U16;
 import net.imagej.Dataset;
 import net.imagej.ImageJ;
 import net.imagej.ops.OpService;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.img.Img;
 import net.imglib2.type.numeric.RealType;
+import org.apache.log4j.pattern.LogEvent;
+import org.scijava.ItemVisibility;
 import org.scijava.command.Command;
+import org.scijava.log.LogService;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 import org.scijava.ui.UIService;
 
 import java.io.File;
+import java.sql.SQLTransactionRollbackException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,41 +38,66 @@ import java.util.List;
  * and replace the {@link `run`} method implementation with your own logic.
  * </p>
  */
-@Plugin(type = Command.class, menuPath = "Plugins>QCMPCompressCommand")
+@Plugin(type = Command.class, menuPath = "Plugins>QCMP Compression")
 public class QCMPCompressCommand<T extends RealType<T>> implements Command {
-    //
-    // Feel free to add more parameters here...
-    //
+
+    // Different quantization methods.
+    private static final String SQ = "Scalar";
+    private static final String VQ1D = "Row Vector";
+    private static final String VQ2D = "Matrix Vector";
+
+    // Different codebook types.
+    private static final String Individual = "Individual";
+    private static final String MiddlePlane = "Middle plane";
+    private static final String Global = "Global";
+
+    @Parameter(label = "Input image or dataset")
+    private File inputImageFile;
+
+    @Parameter(label = "Quantization method", choices = {SQ, VQ1D, VQ2D})
+    private String quantizationMethod = VQ2D;
+
+    @Parameter(label = "Quantization codebook size", choices = {"4", "8", "16", "32", "64", "128", "256"})
+    private String codebookSize = "256";
+
+    @Parameter(label = "Vector width", min = "1", max = "50")
+    private int vectorWidth = 3;
+
+    @Parameter(label = "Vector height", min = "1", max = "50", required = false)
+    private int vectorHeight = 3;
+
+    @Parameter(label = "Codebook type", choices = {Individual, MiddlePlane, Global})
+    private String codebookType = Individual;
+
+    @Parameter(label = "Codebook cache", required = false)
+    private File codebookCacheDirectory;
 
     @Parameter
-    private Dataset currentData;
-
-    @Parameter
-    private UIService uiService;
-
-    @Parameter
-    private OpService opService;
+    private LogService logger;
 
     @Override
     public void run() {
-        final Img<T> image = (Img<T>)currentData.getImgPlus();
+        logger.info("Input file " + inputImageFile.getPath());
+        logger.info("Quantization method: " + quantizationMethod + "(" + codebookType + " codebook, L = " + codebookSize + ")");
+        logger.info(String.format("Vector dimensions [%dx%d]", vectorWidth, vectorHeight));
 
+        //        final Img<T> image = (Img<T>) currentData.getImgPlus();
+        //        //
+        //        // Enter image processing code here ...
+        //        // The following is just a Gauss filtering example
+        //        //
+        //        final double[] sigmas = {1.0, 3.0, 5.0};
         //
-        // Enter image processing code here ...
-        // The following is just a Gauss filtering example
+        //        List<RandomAccessibleInterval<T>> results = new ArrayList<>();
         //
-        final double[] sigmas = {1.0, 3.0, 5.0};
-
-        List<RandomAccessibleInterval<T>> results = new ArrayList<>();
-
-        for (double sigma : sigmas) {
-            results.add(opService.filter().gauss(image, sigma));
-        }
-
-        // display result
-        for (RandomAccessibleInterval<T> elem : results) {
-            uiService.show(elem);
-        }
+        //        for (double sigma : sigmas) {
+        //            results.add(opService.filter().gauss(image, sigma));
+        //        }
+        //
+        //        // display result
+        //        for (RandomAccessibleInterval<T> elem : results) {
+        //            uiService.show(elem);
+        //        }
     }
 
     /**
@@ -83,19 +113,19 @@ public class QCMPCompressCommand<T extends RealType<T>> implements Command {
         final ImageJ ij = new ImageJ();
         ij.ui().showUI();
 
+
         // ask the user for a file to open
-        final File file = ij.ui().chooseFile(null, "open");
-
-        if (file != null) {
-            // load the dataset
-            final Dataset dataset = ij.scifio().datasetIO().open(file.getPath());
-
-            // show the image
-            ij.ui().show(dataset);
-
-            // invoke the plugin
-            ij.command().run(QCMPCompressCommand.class, true);
-        }
+        //        final File file = ij.ui().chooseFile(null, "open");
+        //
+        //        if (file != null) {
+        //            // load the dataset
+        //            final Dataset dataset = ij.scifio().datasetIO().open(file.getPath());
+        //
+        //            // show the image
+        //            ij.ui().show(dataset);
+        //
+        //            // invoke the plugin
+        //            ij.command().run(QCMPCompressCommand.class, true);
+        //        }
     }
-
 }
